2be63a52b4cef80fd34747c3ec3a9169c9534438
summer4requiem <summer.requiem96@gmail.com>
2020-03-18 16:14:53 +0300

    fixed7

--- js/utils.js
+++ js/utils.js
@@ -6,13 +6,9 @@
     return Math.floor(Math.random() * (max - min) + min);
   };
 
-  // var getRandomElement = function (array) {
-  //   return array[getRandomNumber(0, array.length)];
-  // };
 
   window.utils = {
     getRandomNumber: getRandomNumber,
-    // getRandomElement: getRandomElement,
     ESC_KEY: ESC_KEY
   };
 
